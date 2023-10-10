f860aa6a20af8fe4e5bae5d757c5d322736950aa
Rifqi Ziyad Imtinan <rifqiimtinan@gmail.com>
2023-10-10 19:11:18 +0700

    finishing

--- Backend/app/routes/index.js
+++ Backend/app/routes/index.js
@@ -1,29 +1,48 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 const fs = require("fs");
+const path = require("path");
 
 /* GET home page. */
 router.get("/", (req, res, next) => {
-  console.clear();
-
+  console.log("---------------------------------------------------");
   const queryParams = req.query.path;
   let Resdata = readDir(queryParams);
 
   res.send(Resdata);
 });
 
-const readDir = (path) => {
-  //  *Membaca Daftar File dalam Sebuah Direktori
-  const files = fs.readdirSync(path, (err, files) => {
-    if (err) {
-      console.error("Gagal membaca direktori:", err);
-      return;
-    }
+const readDir = (dir) => {
+  try {
+    const files = fs.readdirSync(dir, (err, files) => {
+      console.log(err);
+      return files;
+    });
 
-    return files;
-  });
+    let reqData = [];
 
-  return files;
+    files.forEach((data) => {
+      const filePath = path.join(dir, data);
+      const fileStat = fs.statSync(filePath);
+      if (fileStat.isDirectory()) {
+        reqData.push({
+          name: data,
+          type: "dir",
+        });
+      }
+      if (fileStat.isFile()) {
+        reqData.push({
+          name: data,
+          type: "file",
+        });
+      }
+    });
+
+    return { data: reqData, message: "success" };
+  } catch (error) {
+    console.log(error);
+    return { data: [], message: error.message, status: 400 };
+  }
 };
 
 module.exports = router;
